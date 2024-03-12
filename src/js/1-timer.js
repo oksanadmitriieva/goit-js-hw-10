57197040a1756ebf86f0727e9c58b2e7a07f3a0b
oksanadmitriieva <145439444+oksanadmitriieva@users.noreply.github.com>
2024-03-12 12:55:04 +0100

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,7 +3,7 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+
 
 const input = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
